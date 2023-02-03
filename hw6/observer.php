50c351a5922d93202990ddf3644817b7870676ec
Katherine <strix1112@gmail.com>
2023-02-03 15:56:07 +0300

    homework 6

---
+++ hw6/observer.php
@@ -0,0 +1,120 @@
+<?php
+interface ObserverInterface
+{
+    public function update(ObservableInterface $observable);
+}
+interface ObservableInterface
+{
+    // Добавление нового наблюдателя
+    public function attach(ObserverInterface $observer);
+
+    // Удаление имеющегося наблюдателя
+    public function detach(ObserverInterface $observer);
+
+    // Оповещение всех наблюдателей через вызов у него метода update
+    public function notify();
+}
+
+class Vacancy implements ObservableInterface
+{
+    private array $observers = [];
+
+    private string $name;
+
+    public function attach(ObserverInterface $observer)
+    {
+        foreach ($this->observers as $object){
+            if($object === $observer){
+                return false;
+            }
+        }
+        $this->observers[] = $observer;
+        return true;
+    }
+
+    public function detach(ObserverInterface $observer)
+    {
+        foreach ($this->observers as $key => $object){
+            if($observer === $object){
+                unset($this->observers[$key]);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function notify()
+    {
+        foreach ($this->observers as $observer){
+            $observer->update($this);
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        $this->notify();
+    }
+}
+
+class Hunter1 implements ObserverInterface
+{
+    public function __construct(
+        private string $name
+    )
+    {
+    }
+    private string $currentVacancy;
+
+    public function update(ObservableInterface $observable)
+    {
+        $this->currentVacancy = $observable->getName();
+        $this->display();
+    }
+
+    public function display()
+    {
+        echo $this->name . ": Вакансия -  " . $this->currentVacancy . PHP_EOL;
+    }
+}
+class Hunter2 implements ObserverInterface
+{
+    public function __construct(
+        private string $name
+    )
+    {
+    }
+
+    private string $currentVacancy;
+
+    public function update(ObservableInterface $observable)
+    {
+        $this->currentVacancy = $observable->getName();
+        $this->display();
+    }
+
+    public function display()
+    {
+        echo $this->name . ": Вакансия -  " . $this->currentVacancy . PHP_EOL;
+    }
+}
+
+$h1 = new Hunter1('Иван');
+$h2 = new Hunter2('Николай');
+$vacancy = new Vacancy();
+
+$vacancy->attach($h1);
+$vacancy->attach($h2);
+$vacancy->setName('New vacancy');
+$vacancy->setName('PHP-programmer');
+$vacancy->detach($h1);
+$vacancy->setName('Frontend developer');
+
+
+
+
---
+++ hw6/strategy.php
@@ -0,0 +1,84 @@
+<?php
+class Socks
+{
+    public function __construct(
+        private int $count,
+        private int $price
+    )
+    {
+    }
+    /**
+     * @return int
+     */
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+}
+// Создаем интерфейс и три стратегии
+interface PaymentInterface
+{
+    public function payment(): string;
+}
+
+class QiwiPayment implements PaymentInterface
+{
+    public function payment(): string
+    {
+        return "Оплата Qiwi";
+    }
+}
+
+
+class WebMoneyPayment implements PaymentInterface
+{
+    public function payment(): string
+    {
+        return "Оплата WebMoney";
+    }
+}
+class YandexPayment implements PaymentInterface
+{
+    public function payment(): string
+    {
+        return "Оплата Яндекс.Деньги";
+    }
+}
+
+// Создаем объект Cart  в который внедряем стратегию метода оплаты
+class Cart
+{
+    private array $socks = [];
+
+    public function addToCart(Socks $socks): void
+    {
+        $this->socks[] = $socks;
+    }
+    public function getSocks(): array
+    {
+        return $this->socks;
+    }
+    public function payment(PaymentInterface $payment, array $socks): string
+    {
+        return $payment->payment();
+    }
+
+}
+
+// Использование
+$socks = new Socks(10, 150);
+$cart = new Cart();
+$cart->addToCart($socks);
+
+echo $cart->payment(new QiwiPayment(), $cart->getSocks()) . PHP_EOL;
+echo $cart->payment(new WebMoneyPayment(), $cart->getSocks()) . PHP_EOL;
+echo $cart->payment(new YandexPayment(), $cart->getSocks()) . PHP_EOL;
+
